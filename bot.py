57e9758e44b004037e6556469eed863e03b09d58
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-08-19 11:31:42 +0500

    full code

--- bot.py
+++ bot.py
@@ -1,19 +1,77 @@
 import requests
+import time
+TOKEN = "6565529385:AAEAcOoC3SkHhoK0pYMGdhKYppspW591Nws"
+BASE_URL = "https://api.telegram.org/bot"
 
 
-BASE_URL = 'https://api.telegram.org/bot'
-TOKEN    = "6602447524:AAFuq2qNvn61P7j5ocEdDxoRn0jw8TVAO4Y"
-
-
-def getMe() -> dict:
-    '''getting info about the bot'''
-    url = f"{BASE_URL}{TOKEN}/getMe"
+def send_photo(chat_id: str, photo: str) -> None:
+    '''send photo to tg user'''
+    url = f"{BASE_URL}{TOKEN}/sendPhoto"
+    payload = {
+        "chat_id": chat_id,
+        "photo": photo,
+        "caption": "*this is a dog's photo*",
+        "parse_mode": "MarkdownV2"
+    }
+    requests.get(url, params=payload)
+    
+def sendMessage(chat_id: str, text: str) ->None:
+    '''send message to tg user'''
+    url=f"{BASE_URL}{TOKEN}/sendMessage"
+    payload={
+        'chat_id':chat_id,
+        'text': text
+    }
+    response=requests.get(url,params=payload)
+        
+    
+def last_update() -> list[dict]:
+    '''get last updates'''
+    url = f"{BASE_URL}{TOKEN}/getUpdates"
 
     response = requests.get(url)
 
     if response.status_code == 200:
-        return response.json()
-    
+        return response.json()['result'][-1]
+
     return False
 
-print(getMe())
+
+def dog_photo() -> str:
+    url = 'https://random.dog/woof.json'
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()['url']
+
+    return
+
+
+def main():
+    last_update_id = last_update()['update_id']
+
+    while True:
+        time.sleep(0.5)
+
+
+        curr_update = last_update()
+
+        # if last_update_id!=curr_update['update_id']:
+        #     chat_id=curr_update['message']['chat']['id']
+        #     text=curr_update['message']['text']
+        #     sendMessage(chat_id,text)
+        # last_update_id=curr_update['update_id']
+
+        if last_update_id == curr_update['update_id']:
+            continue
+
+        text = curr_update['message']['text']
+        if text == '/dog':
+            chat_id = curr_update['message']['chat']['id']
+            photo = dog_photo()
+            send_photo(chat_id, photo)
+        else:
+            chat_id=curr_update['message']['chat']['id']
+            sendMessage(chat_id,text)
+        last_update_id = curr_update['update_id']
+main()
